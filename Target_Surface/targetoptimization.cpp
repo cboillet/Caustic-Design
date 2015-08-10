339f48bb202a5f4cb134b4cb6152b4d3f560ef63
Patrick <p.engelhard@mailbox.tu-berlin.de>
2015-08-10 15:01:15 +0200

    ceres array handling
    Signed-off-by: Patrick <p.engelhard@mailbox.tu-berlin.de>

--- Target_Surface/targetoptimization.cpp
+++ Target_Surface/targetoptimization.cpp
@@ -6,11 +6,21 @@ using ceres::Problem;
 using ceres::Solver;
 using ceres::Solve;
 
-struct CostFunctor {
-  template <typename T> bool operator()(const T* const x, T* residual) const {
-    residual[0] = T(10.0) - x[0];
-    return true;
-  }
+class MyCostFunctor{
+public:
+    MyCostFunctor(int numX): numX(numX){}
+
+    template <typename T>
+    bool operator()(const T* const x, T* e) const{
+        for (int i=0; i<numX; i++)
+        {
+            e[0] += T(10.0) - x[i];
+        }
+        return true;
+    }
+
+private:
+    int numX;
 };
 
 TargetOptimization::TargetOptimization()
@@ -20,23 +30,28 @@ TargetOptimization::TargetOptimization()
 void TargetOptimization::runCeresTest()
 {
 
-    double x = 0.5;
-    const double initial_x = x;
+    double *x = new double[1];
+    x[0] = 3;
+    //x[1] = 2;
+    double *initial_x = new double[1];
+    initial_x[0] = 3;
 
     Problem problem;
 
     CostFunction* cost_function =
-        new AutoDiffCostFunction<CostFunctor, 1, 1>(new CostFunctor);
-    problem.AddResidualBlock(cost_function, NULL, &x);
+        new AutoDiffCostFunction<MyCostFunctor, 1, ceres::DYNAMIC>(new MyCostFunctor(1));
+    problem.AddResidualBlock(cost_function, NULL, x);
 
     // Run the solver!
     Solver::Options options;
-    options.minimizer_progress_to_stdout = true;
+    //options.minimizer_progress_to_stdout = true;
     Solver::Summary summary;
     Solve(options, &problem, &summary);
 
     std::cout << summary.BriefReport() << "\n";
-    std::cout << "x : " << initial_x
-              << " -> " << x << "\n";
+    std::cout << "x[0] : " << initial_x[0] //<< ", x[1]:" << initial_x[1]
+              << " -> " << x[0] << /*", " << x[1] << */std::endl;
 
+    //delete[] x;
+    //delete[] initial_x;
 }
