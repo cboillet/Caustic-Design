8bcfa0a4fc56a2317af59ee1b1462321cb9566b6
Patrick <p.engelhard@mailbox.tu-berlin.de>
2015-08-21 11:12:09 +0200

    removed unused argument
    Signed-off-by: Patrick <p.engelhard@mailbox.tu-berlin.de>

--- Target_Surface/mainwindow.cpp
+++ Target_Surface/mainwindow.cpp
@@ -67,7 +67,7 @@ void MainWindow::on_actionGenerateTriangles_triggered()
 
 void MainWindow::on_actionRunTargetOptimization_triggered()
 {
-    optimizer->runOptimization(&(viewer->model), viewer);
+    optimizer->runOptimization(&(viewer->model));
 }
 
 void MainWindow::on_actionLoadLightRayReceiverPosition_triggered()
