add627545bdf280b7d6cb8ef28388bf8f35878c5
Patrick <p.engelhard@mailbox.tu-berlin.de>
2015-08-21 11:10:11 +0200

    Some visualization updates
    Signed-off-by: Patrick <p.engelhard@mailbox.tu-berlin.de>

--- Target_Surface/mainwindow.cpp
+++ Target_Surface/mainwindow.cpp
@@ -67,7 +67,7 @@ void MainWindow::on_actionGenerateTriangles_triggered()
 
 void MainWindow::on_actionRunTargetOptimization_triggered()
 {
-    optimizer->runOptimization(&(viewer->model));
+    optimizer->runOptimization(&(viewer->model), viewer);
 }
 
 void MainWindow::on_actionLoadLightRayReceiverPosition_triggered()
@@ -96,6 +96,17 @@ void MainWindow::on_actionDesired_Normals_toggled(){
     viewer->update();
 }
 
+void MainWindow::on_actionDesired_Rays_toggled()
+{
+    viewer->toggleDrawDesiredRays();
+    viewer->update();
+}
+
+void MainWindow::on_actionDesired_Ray_Directions_toggled()
+{
+    viewer->toggleDrawDesiredRayDirections();
+    viewer->update();
+}
 
 void MainWindow::newMeshHeight()
 {
